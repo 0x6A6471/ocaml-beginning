c8e2e23e98a23f3acafc43698bf6996c24b2d113
0x6A6471 <hello@jakequinter.io>
2025-01-08 17:13:12 -0600

    c2

--- test/test_chapter03.ml
+++ test/test_chapter03.ml
@@ -5,7 +5,12 @@ let test_c1 () =
   Alcotest.(check bool) "not false" true (C1.not false)
 ;;
 
+let test_c2 () =
+  Alcotest.(check int) "sum 5" 15 (C2.sum 5);
+  Alcotest.(check int) "sum 10" 55 (C2.sum 10)
+;;
+
 let tests =
   let open Alcotest in
-  [ test_case "not" `Quick test_c1 ]
+  [ test_case "not" `Quick test_c1; test_case "sum" `Quick test_c2 ]
 ;;
