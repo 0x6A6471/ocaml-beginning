642e33f41de040d062393de37d00624bcbc2042d
0x6A6471 <hello@jakequinter.io>
2025-01-10 17:07:11 -0600

    4.2 comments

--- lib/chapter04/e2.ml
+++ lib/chapter04/e2.ml
@@ -1,9 +1,12 @@
+(* bool list -> int *)
 let rec count_true = function
   | [] -> 0
   | true :: tl -> 1 + count_true tl
   | false :: tl -> count_true tl
 ;;
 
+(* tail recursive. 
+   bool list -> int *)
 let count_true' lst =
   let rec aux count = function
     | [] -> count
