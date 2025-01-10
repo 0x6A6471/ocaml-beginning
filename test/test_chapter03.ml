2ad3db5f6b3538e43b91ea97aa62370efcbf8da3
0x6A6471 <hello@jakequinter.io>
2025-01-10 16:12:35 -0600

    update test fn names

--- test/test_chapter03.ml
+++ test/test_chapter03.ml
@@ -1,16 +1,16 @@
 open Chapter03
 
-let test_c1 () =
+let test_e1 () =
   Alcotest.(check bool) "not true" false (E1.not true);
   Alcotest.(check bool) "not false" true (E1.not false)
 ;;
 
-let test_c2 () =
+let test_e2 () =
   Alcotest.(check int) "sum 5" 15 (E2.sum 5);
   Alcotest.(check int) "sum 10" 55 (E2.sum 10)
 ;;
 
-let test_c3 () =
+let test_e3 () =
   Alcotest.(check int) "power 0 1" 0 (E3.power 0 1);
   Alcotest.(check int) "power 2 0" 1 (E3.power 2 0);
   Alcotest.(check int) "power 1 1" 1 (E3.power 1 1);
@@ -18,9 +18,9 @@ let test_c3 () =
   Alcotest.(check int) "power 5 5" 3125 (E3.power 5 5)
 ;;
 
-let test_c5 () = Alcotest.(check int) "run" 5 E5.run
+let test_e5 () = Alcotest.(check int) "run" 5 E5.run
 
-let test_c6 () =
+let test_e6 () =
   Alcotest.(check bool) "is_lower 'a'" true (E6.is_lower 'a');
   Alcotest.(check bool) "is_lower 'A'" false (E6.is_lower 'A');
   Alcotest.(check bool) "is_upper 'a'" false (E6.is_upper 'a');
@@ -29,10 +29,10 @@ let test_c6 () =
 
 let tests =
   let open Alcotest in
-  [ test_case "not" `Quick test_c1
-  ; test_case "sum" `Quick test_c2
-  ; test_case "power" `Quick test_c3
-  ; test_case "run" `Quick test_c5
-  ; test_case "lower upper fns" `Quick test_c6
+  [ test_case "not" `Quick test_e1
+  ; test_case "sum" `Quick test_e2
+  ; test_case "power" `Quick test_e3
+  ; test_case "run" `Quick test_e5
+  ; test_case "lower upper fns" `Quick test_e6
   ]
 ;;
