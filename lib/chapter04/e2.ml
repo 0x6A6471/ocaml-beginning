b1cee2ef6fba40f69be25813a5e459b4e0f91ff3
0x6A6471 <hello@jakequinter.io>
2025-01-10 17:05:29 -0600

    4.2

---
+++ lib/chapter04/e2.ml
@@ -0,0 +1,14 @@
+let rec count_true = function
+  | [] -> 0
+  | true :: tl -> 1 + count_true tl
+  | false :: tl -> count_true tl
+;;
+
+let count_true' lst =
+  let rec aux count = function
+    | [] -> count
+    | true :: tl -> aux (count + 1) tl
+    | false :: tl -> aux 0 tl
+  in
+  aux 0 lst
+;;
--- test/test_chapter04.ml
+++ test/test_chapter04.ml
@@ -4,7 +4,12 @@ let test_e1 () =
   Alcotest.(check (list int)) "evens [2; 4; 2; 4; 2]" [ 4; 4 ] (E1.evens [ 2; 4; 2; 4; 2 ])
 ;;
 
+let test_e2 () =
+  Alcotest.(check int) "count_true [true; false; true]" 2 (E2.count_true [ true; false; true ]);
+  Alcotest.(check int) "count_true' [true; false; true]" 2 (E2.count_true' [ true; false; true ])
+;;
+
 let tests =
   let open Alcotest in
-  [ test_case "evens" `Quick test_e1 ]
+  [ test_case "evens" `Quick test_e1; test_case "count_true" `Quick test_e2 ]
 ;;
