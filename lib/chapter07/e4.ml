c982e32f93c114c5295daa16bd55f9d16b25bc30
0x6A6471 <hello@jakequinter.io>
2025-02-27 18:28:31 -0600

    7.4

--- lib/chapter07/e4.ml
+++ lib/chapter07/e4.ml
@@ -1,5 +1,4 @@
 let largest_int_or_zero n =
-  match n with
-  | n when n < 0 -> 0
-  | _ -> float_of_int n |> sqrt |> floor |> int_of_float
+  try E3.largest_int_sqrt n with
+  | E3.NegativeSqRt -> 0
 ;;
